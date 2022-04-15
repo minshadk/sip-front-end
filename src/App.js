4bc7919efc7946da7bd91260a97065ebf59e6f04
minshadk <minshadk13@gmail.com>
2022-04-15 23:29:41 +0530

    product page created

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route, Navigate, BrowserRouter } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import AppNavBar from "./components/utils/Navigation/NavigationBar/AppNavBar";
-import Product from "./components/pages/Product";
+import Products from "./components/pages/Products";
 import Events from "./components/pages/Events";
 import Login from "./components/pages/Login";
 import SingUp from "./components/pages/SingUp";
@@ -14,7 +14,8 @@ import ServiceRequestAdmin from "./components/pages/Admin/ServiceRequestAdmin";
 import DonareAdmin from "./components/pages/Admin/DonareAdmin";
 import BloodDonationAdmin from "./components/pages/Admin/BloodDonationAdmin";
 import AddProduct from "./components/pages/Admin/AddProduct";
-import SingleProductAdmin from './components/pages/Admin/SingleProductAdmin';
+import SingleProductAdmin from "./components/pages/Admin/SingleProductAdmin";
+import Product from "./components/pages/Product";
 
 function App() {
   return (
@@ -22,12 +23,13 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/singup" element={<SingUp />} />
-        {/* <Route path="/" element={<AppNavBar />}>
+        <Route path="/" element={<AppNavBar />}>
           <Route path="/home" element={<Home />} />
           <Route path="/product" element={<Product />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/events" element={<Events />} />
-        </Route> */}
-        <Route path="/" element={<SideBar />}>
+        </Route>
+        {/* <Route path="/" element={<SideBar />}>
           <Route path="/product" element={<ProductAdmin />} />
           <Route path="/productOrder" element={<ProductOrder />} />
           <Route path="/service" element={<ServiceAdmin />} />
@@ -40,7 +42,7 @@ function App() {
           <Route path="/addProduct" element={<AddProduct />} />
           <Route path="/singleProductAdmin" element={<SingleProductAdmin />} />
 
-        </Route>
+        </Route> */}
       </Routes>
     </BrowserRouter>
   );
--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -1,73 +1,53 @@
 import React from "react";
 
-import { Box, Container, Grid, Typography } from "@mui/material";
-
-//  Importing Custom Components
+import { Box, Container, Grid, Stack, Typography, Button } from "@mui/material";
 import { PageWrapper } from "../utils/PageWrapper";
-import ServicesCard from "../utils/Cards/ServicesCard";
-import EventsCard from "../utils/Cards/EventsCard";
-
-// Importing images
 import HelpingOthers from "../../Assets/Images/helpingOthers.png";
 
+//  Importing Custom Components
+// import { PageWrapper } from "../utils/PageWrapper";
+
+// import { PageWrapper } from "../../utils/PageWrapper";
+// import HelpingOthers from "../../../Assets/Images/helpingOthers.png";
 export default function Product() {
   return (
     <PageWrapper>
       <Container maxWidth="xl">
-        <Box>
-          <Typography
-            variant="h2"
-            sx={{
-              marginTop: 25,
-              marginBottom: 25
-            }}
-          >
-            we rise by liftng others
-          </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <img
+            src={HelpingOthers}
+            //   alt={item.title}
+          />
         </Box>
-        <Grid container>
-          <Grid item xs={12} sm={4} md={4} lg={6}>
-            <Typography variant="h6" sx={{ paddingTop: 4 }}>
-              We provide to you the best choiches for you. We provide you the
-              necassary needs for your emergency . You can get hand from us at
-              any time at any where
-            </Typography>
+        <Stack
+          direction="row"
+          justifyContent="flex-end"
+          alignItems="center"
+          spacing={2}
+          sx={{ marginY: 5 }}
+        >
+          <Typography>Minimum Order</Typography>
+          <Typography>10</Typography>
+          <Button variant="contained">Buy Now</Button>
+        </Stack>
+        <Box>
+          <Grid sx={{ fontSize: 30 }}>Product Name</Grid>
+          <Grid>
+            Product description img elements must have an alt prop, either with
+            meaningful text, or an empty string for decorative images
+            jsx-a11y/alt-text Line 36:21: img elements must have an alt prop,
+            either with meaningful text, or an empty string for decorative
+            images jsx-a11y/alt-text
           </Grid>
-          <Grid item xs={12} sm={4} md={4} lg={6}>
-            <img src={HelpingOthers} height="400px" />
-          </Grid>
-          <Box>
-            <Grid
-              container
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              sx={{ paddingY: 8 }}
-            >
-              <Grid item>
-                <Grid
-                  container
-                  direction="row"
-                  justifyContent="center"
-                  alignItems="center"
-                  spacing={{ xs: 2, md: 3, lg: 30 }}
-                >
-                  <Grid item xs={12} sm={4} md={4} lg={4}>
-                    <EventsCard />
-                  </Grid>
-                  <Grid item xs={12} sm={4} md={4} lg={4}>
-                    <EventsCard />
-                  </Grid>
-                  <Grid item xs={12} sm={4} md={4} lg={4}>
-                    <EventsCard />
-                  </Grid>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Box>
-        </Grid>
+        </Box>
       </Container>
-      <Box sx={{ backgroundColor: "#1380c2", minHeight: 300 }}></Box>
     </PageWrapper>
   );
 }
